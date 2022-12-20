dc618a44cd2d270e60256dec76bd7f8543b8abc5
Sivambika Duraisingam <mailsivambika@gmail.com>
2022-12-20 15:28:07 -0500

    initial commit

---
+++ src/test/java/com/example/demo/controllers/ItemObjectUtil.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Item;
+
+import java.math.BigDecimal;
+
+
+public class ItemObjectUtil {
+
+    public static final String ROUND_WIDGET = "Round Widget";
+
+    public static final String SQUARE_WIDGET = "Square Widget";
+
+    public static Item getItem0() {
+        Item item = new Item();
+        item.setId(0L);
+        item.setName(ROUND_WIDGET);
+        item.setPrice(new BigDecimal("2.99"));
+        item.setDescription("A widget that is round");
+        return item;
+    }
+
+    public static Item getItem1() {
+        Item item = new Item();
+        item.setId(1L);
+        item.setName(SQUARE_WIDGET);
+        item.setPrice(new BigDecimal("1.99"));
+        item.setDescription("A widget that is square");
+        return item;
+    }
+}
